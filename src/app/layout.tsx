551a530f8e18629d3aa543d06541169f28b0a04b
codemara <116239909+codemara@users.noreply.github.com>
2024-01-02 09:08:13 -0700

    Changes to be committed: 	new file:   .eslintrc.json 	new file:   .gitattributes 	new file:   .gitignore 	new file:   global.d.ts 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/beams.png 	new file:   public/built.jpg 	new file:   public/codemara.svg 	new file:   public/field.jpg 	new file:   public/final.png 	new file:   public/leather.png 	new file:   public/material.png 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   src/SvgController.ts 	new file:   src/SvgMargin.ts 	new file:   src/app/box-css-module/NavBox.svg 	new file:   src/app/box-css-module/NavBoxMain.svg 	new file:   src/app/box-css-module/box.module.css 	new file:   src/app/favicon.ico 	new file:   src/app/globals.css 	new file:   src/app/headers-menu/page.tsx 	new file:   src/app/layout.tsx 	new file:   src/app/page.tsx 	new file:   src/app/phoneIcon.tsx 	new file:   tailwind.config.ts 	new file:   tsconfig.json

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import Headersmenu from './headers-menu/page';
+
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'codeMARA',
+  description: 'Web and I.T. solutions by Noemi Desmarais',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+    <body>
+          <Headersmenu />
+          {children}
+      </body>
+    </html>
+  );
+}
+
+
