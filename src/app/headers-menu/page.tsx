8bc157e71d5320e25f49b56c9bef8952b301fbb9
codemara <116239909+codemara@users.noreply.github.com>
2024-01-03 09:58:27 -0700

    Update page.tsx

--- src/app/headers-menu/page.tsx
+++ src/app/headers-menu/page.tsx
@@ -20,14 +20,14 @@ function classNames(...classes: string[]) {
 }
 
 export default function headersmenu() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <header id="headerMenu" className="z-50 bg-white fixed top-0 w-full hidden">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">codeMara</span>
+            <span className="sr-only">codeMARA</span>
             <img className="h-12 w-auto invert" src="/codemara.svg" alt="" />
           </a>
         </div>
