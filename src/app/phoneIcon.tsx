d55da79fb26941b65f6f54f47282580dcc841ed8
codemara <116239909+codemara@users.noreply.github.com>
2024-01-03 18:00:24 -0700

    Fix sticky menu

--- src/app/phoneIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useRef } from 'react';
-  // import { IconsType } from './icons.type';
-  // import { ColorsType } from '@/styles/colors.type';
-interface IconProps extends React.SVGAttributes<HTMLOrSVGElement> {
- // name: IconsType;
-  width: number;
-  height: number;
- // color?: ColorsType;
-}
