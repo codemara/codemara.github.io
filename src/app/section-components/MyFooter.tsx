02fc7ca549b8ce05a91a018ce9e42ff92839bd55
Noemi Desmarais <116239909+codemara@users.noreply.github.com>
2026-08-31 12:41:56 -0600

    Update MyFooter.tsx

--- src/app/section-components/MyFooter.tsx
+++ src/app/section-components/MyFooter.tsx
@@ -7,15 +7,15 @@ export default function MyFooter() {
         <><footer className="bg-gray-100 dark:bg-gray-700">
             <div className="mx-auto w-full max-w-screen-xl">
                 <div className="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
-                    <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2024 Noemi Desmarais
+                    <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2026 Noemi Desmarais
                     </span>
 
                     <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
-                        <a href="tel:5142434627"
+                        <a href="tel:8258834627"
                             className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100  dark:text-gray-300"
                         >
                             <AiOutlinePhone className="h-5 w-5 flex-none text-gray-400 hover:text-gray-900 dark:text-gray-300" aria-hidden="true" />
-                            (514) 243-4627
+                            (825) 883-4627
                         </a>
 
                         <a href="http://www.github.com/codemara"
